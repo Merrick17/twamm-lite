e30738ead78d99d2b47adb841395e3d9efcc2035
aperire <aperire0402@gmail.com>
2023-07-21 02:58:33 -0400

    a

--- src/library.tsx
+++ src/library.tsx
@@ -19,7 +19,7 @@ const scriptDomain =
       // eslint-disable-next-line consistent-return
       return new URL(url).origin;
     }
-  })() || "https://lite.lp.finance";
+  })() || "https://twamm-lite-ba91.vercel.app/";
 
 async function loadRemote(
   id: string,
